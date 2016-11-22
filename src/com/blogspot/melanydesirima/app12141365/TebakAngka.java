ab5cd0841d9c124265cfa6b77c62679cdb000141
desirimamelany <melanydesirima@yahoo.com>
2016-11-22 11:47:09 +0700

    12141365 Menambahkan fungsi do...while. Do ... untuk menginput nama pemain (palyer) dengan menggunakan scanner input1. Lalu while untuk memberikan perintah keluar dari game (exit).

--- src/com/blogspot/melanydesirima/app12141365/TebakAngka.java
+++ src/com/blogspot/melanydesirima/app12141365/TebakAngka.java
@@ -12,12 +12,21 @@ import java.util.Random;
 
 public class TebakAngka {
 public static void main(String[] args) {
-        ArrayList <String> player = new ArrayList<>();
-        ArrayList <Integer> score = new ArrayList<>();
-        Scanner input1 = new Scanner(System.in);
-        Scanner input2= new Scanner(System.in);
-        Random acakAngka=new Random();
+    ArrayList <String> player = new ArrayList<>();
+    ArrayList <Integer> score = new ArrayList<>();
+    Scanner input1 = new Scanner(System.in);
+    Scanner input2= new Scanner(System.in);
+    Random acakAngka=new Random();
         
+        String Y;
+        do{
+            System.out.println("Masukkan 'Nama' anda atau tekan 'y' untuk keluar, kemudian tekan enter : ");
+            Y=input1.nextLine();
+            player.add(Y);
+                
+            
+        }        
+        while(!"y".equals(Y));
         
     }
 }
