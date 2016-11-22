f31e4ab7264ad8af76ed425f704c1fe47ed49352
desirimamelany <melanydesirima@yahoo.com>
2016-11-22 11:25:50 +0700

    12141365 Membuat arraylist player (pemain game) dan score (nilai game). Arraylist berada pada package java.util.ArrayList. ArrayList dapat menangani data yang bersifat pasti dan digunakan untuk menampung sebuah data dari program sederhana. Pada java dengan menggunakan ArrayList dapat menentukan jenis dari elemen (sebuah class).

--- src/com/blogspot/melanydesirima/app12141365/TebakAngka.java
+++ src/com/blogspot/melanydesirima/app12141365/TebakAngka.java
@@ -1,7 +1,17 @@
 package com.blogspot.melanydesirima.app12141365;
 
+/**
+ * UTS Java Semester 5
+ * TI Malam - 12141365
+ * @author Desi Rima Melany
+ */
+
+import java.util.ArrayList;
+
 public class TebakAngka {
 public static void main(String[] args) {
+        ArrayList <String> player = new ArrayList<>(); // Membuat array Nama Pemain
+        ArrayList <Integer> score = new ArrayList<>(); // Membuat array Sekor Pemain
         
     }
 }
